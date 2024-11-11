3f8c9fb294ee679c28c99407c605a6aeaa02c4b1
Erik Hemming <eriquesnk@gmail.com>
2024-11-11 23:23:27 +0100

    patch tundra to enable building on arm

--- build_tundra.sh
+++ build_tundra.sh
@@ -7,5 +7,6 @@ rm -rf tundra t2-build
 echo -e "\033[1;36m****** TUNDRA ******\033[0m"
 
 git clone --depth=1 --recurse-submodules https://github.com/deplinenoise/tundra.git t2-build
+patch -p 0 < tundra.patch # ARM
 cd t2-build && make -j 4 && PREFIX=$PWD/../tundra make install && cd -
 rm -rf t2-build
